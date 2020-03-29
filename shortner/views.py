ed258be728f9ffa663681b15fa32d433083b864d
Madhava2209 <52972972+Madhava2209@users.noreply.github.com>
2020-03-29 14:10:27 +0530

    DONE

--- shortner/views.py
+++ shortner/views.py
@@ -25,19 +25,21 @@ def create_short_url(request):
             title=title,
             long_url=long_url,
             short_hash=m,
-            user=request.user
+            user=request.user,
+            no_clicks=0,
 
         )
         return redirect('/dashboard/')
 
 
 def redirect_to_long_url(request,hashcode):
-    url_instance=url.objects.get(pk=hashcode)
-    no_clicks=url_instance.no_clicks+1
+    url_instance=url.objects.get(short_hash=hashcode)
+    no_clicks=url_instance.no_clicks
+    no_clicks=no_clicks+1
     url_instance.objects.update(no_clicks=no_clicks)
     url_instance.save()
     long_url=url_instance.long_url
-    return redirect("{{/long_url/}}")
+    return redirect(f"/{long_url}/")
 
 
 
